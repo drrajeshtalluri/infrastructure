d1a06cf9c852e103d273fa3821c24af30af24e14
Rajesh Talluri <45318617+drrajeshtalluri@users.noreply.github.com>
2025-04-18 16:37:28 -0700

    Add networking module variables

---
+++ aws/terraform/modules/networking/variables.tf
@@ -0,0 +1,42 @@
+############################
+# Networking Variables
+############################
+
+variable "prefix" {
+  description = "Prefix to be used for all resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "enable_flow_logs" {
+  description = "Enable VPC flow logs"
+  type        = bool
+  default     = true
+}
+
+variable "flow_logs_retention_days" {
+  description = "Number of days to retain VPC flow logs"
+  type        = number
+  default     = 14
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
